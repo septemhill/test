88201ecfcd69fe5b607da20104dc47a97b690a6c
Septem Li <septemhill@gmail.com>
2020-08-31 22:25:19 +0800

    update comments implements

--- module/article.go
+++ module/article.go
@@ -20,11 +20,12 @@ type Article struct {
 }
 
 type Comment struct {
-	ID       int       `db:"id" json:"id"`
-	Author   string    `db:"author" json:"author"`
-	Content  string    `db:"content" json:"content"`
-	CreateAt time.Time `db:"create_at" json:"createAt"`
-	UpdateAt time.Time `db:"update_at" json:"updateAt"`
+	ID        int       `db:"id" json:"id"`
+	ArticleID int       `db:"art_id" json:"art_id"`
+	Author    string    `db:"author" json:"author"`
+	Content   string    `db:"content" json:"content"`
+	CreateAt  time.Time `db:"create_at" json:"createAt"`
+	UpdateAt  time.Time `db:"update_at" json:"updateAt"`
 }
 
 func NewPost(ctx context.Context, db *db.DB, art Article) error {
@@ -74,6 +75,7 @@ func GetPosts(ctx context.Context, db *db.DB, size, offset int, asc bool) ([]Art
 	if err != nil {
 		return nil, err
 	}
+	tx.Commit()
 
 	arts := make([]Article, 0)
 
@@ -93,19 +95,62 @@ func GetPost(ctx context.Context, db *db.DB, art Article) (*Article, error) {
 	return nil, nil
 }
 
-func NewComment(ctx context.Context, db *db.DB, comment Comment) error {
-	return nil
+func NewComment(ctx context.Context, db *db.DB, postID string, comment Comment) error {
+	expr := `INSERT INTO comments VALUES (DEFAULT, $1, $2, $3, $4, $5)`
+
+	return txAction(ctx, db, func(tx *sqlx.Tx) error {
+		if _, err := tx.ExecContext(ctx, expr, postID, comment.Author, comment.Content, time.Now(), time.Now()); err != nil {
+			return err
+		}
+		return nil
+	})
 }
 
 func UpdateComment(ctx context.Context, db *db.DB, comment Comment) error {
-	return nil
+	expr := `UPDATE comments SET content = $1 WHERE id = $2`
+
+	return txAction(ctx, db, func(tx *sqlx.Tx) error {
+		if _, err := tx.ExecContext(ctx, expr, comment.ID); err != nil {
+			return err
+		}
+		return nil
+	})
 }
 
-func GetComments(ctx context.Context, db *db.DB, size, offset int, asc bool) ([]Comment, error) {
-	return nil, nil
+func GetComments(ctx context.Context, db *db.DB, postID string, size, offset int) ([]Comment, error) {
+	expr := `SELECT * FROM comments WHERE art_id = $1 ORDER BY create_at ASC LIMIT $2 OFFSET $3`
+
+	tx, err := db.BeginTxx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	comments := make([]Comment, 0)
+
+	rows, err := tx.QueryxContext(ctx, expr, postID, size, offset)
+	if err != nil {
+		return nil, err
+	}
+	tx.Commit()
+
+	for rows.Next() {
+		comment := Comment{}
+		if err := rows.StructScan(&comment); err != nil {
+			return nil, err
+		}
+		comments = append(comments, comment)
+	}
+
+	return comments, nil
 }
 
 func DeleteComment(ctx context.Context, db *db.DB, comment Comment) error {
-	//expr := `DELETE FROM comments WHERE id = comment`
-	return nil
+	expr := `DELETE FROM comments WHERE id = $1`
+
+	return txAction(ctx, db, func(tx *sqlx.Tx) error {
+		if _, err := tx.ExecContext(ctx, expr, comment.ID); err != nil {
+			return err
+		}
+		return nil
+	})
 }
