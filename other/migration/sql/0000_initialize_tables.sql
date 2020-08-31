642af0e22ecdbd38fc6a40917b8acf62bb882d0e
Septem Li <septemhill@gmail.com>
2020-08-31 22:24:46 +0800

    update migrate SQL

---
+++ other/migration/sql/0000_initialize_tables.sql
@@ -0,0 +1,38 @@
+--liquibase formatted sql
+
+--comment: Initialize tables 
+--changeset Septem: 0001_initialize_tables
+CREATE TABLE IF NOT EXISTS accounts (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	username VARCHAR(30) NOT NULL UNIQUE,
+	email VARCHAR(50) NOT NULL UNIQUE,
+	phone VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS accounts_private (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	username VARCHAR(30) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES accounts(username)
+);
+
+CREATE TABLE IF NOT EXISTS articles (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	author VARCHAR(30) NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	content VARCHAR(10000),
+	create_at TIMESTAMPTZ NOT NULL,
+	update_at TIMESTAMPTZ NOT NULL
+	CONSTRAINT fk_author FOREIGN KEY (author) REFERENCES accounts(username)
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	art_id BIGSERIAL NOT NULL, 
+	author VARCHAR(30) NOT NULL,
+	content VARCHAR(1000) NOT NULL,
+	create_at TIMESTAMPTZ NOT NULL,
+	update_at TIMESTAMPTZ NOT NULL,
+	CONSTRAINT fk_article_id FOREIGN KEY (art_id) REFERENCES articles(id),
+	CONSTRAINT fk_author FOREIGN KEY (author) REFERENCES accounts(username)
+);
--- other/migration/sql/0001_create_drytable.sql
+++ other/migration/sql/0001_create_drytable.sql
@@ -2,7 +2,7 @@
 
 --comment: Create table `drytable`, and add sample data
 --changeset Septem:0001_create_drytable_table_add_sample_data
-CREATE TABLE drytable (
+CREATE TABLE IF NOT EXISTS drytable (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	fname CHAR(25),
 	lname CHAR(25)
