d62ae660118c29e280b27957ba7138110a36f5d5
Septem Li <septemhill@gmail.com>
2020-09-05 08:54:08 +0800

    remove unused function

--- db/db.go
+++ db/db.go
@@ -3,7 +3,6 @@ package db
 import (
 	_ "github.com/jackc/pgx/stdlib"
 	"github.com/jmoiron/sqlx"
-	//_ "github.com/lib/pq"
 )
 
 type DB struct {
@@ -16,18 +15,8 @@ func Connect() *DB {
 	return &DB{DB: db}
 }
 
-//func (db *DB) executeSQL(ctx context.Context, expr string, args ...interface{}) (sql.Result, error) {
-//	return db.ExecContext(ctx, expr, args...)
-//}
-//
-//func (db *DB) Insert(ctx context.Context, expr string, args ...interface{}) (sql.Result, error) {
-//	return db.executeSQL(ctx, expr, args...)
-//}
-//
-//func (db *DB) Update(ctx context.Context, expr string, args ...interface{}) (sql.Result, error) {
-//	return db.executeSQL(ctx, expr, args...)
-//}
-//
-//func (db *DB) Delete(ctx context.Context, expr string, args ...interface{}) (sql.Result, error) {
-//	return db.executeSQL(ctx, expr, args...)
-//}
+func ConnectToTest() *DB {
+	connInfo := `user=septemlee dbname=runtests sslmode=disable`
+	db := sqlx.MustConnect("pgx", connInfo)
+	return &DB{DB: db}
+}
