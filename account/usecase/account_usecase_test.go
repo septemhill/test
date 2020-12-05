aad816a9cbf4a952e0a477d94010454ef9de5d32
Septem Li <septemhill@gmail.com>
2020-12-05 11:06:27 +0800

    update

--- account/usecase/account_usecase_test.go
+++ account/usecase/account_usecase_test.go
@@ -6,14 +6,16 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/septemhill/test/account"
+	"github.com/septemhill/test/account/usecase"
 )
 
 func TestCreate(t *testing.T) {
+	ctx := context.Background()
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	ctx := context.Background()
-
-	uc := account.NewMockAccountUseCase(ctrl)
-	uc.EXPECT().Create(ctx, &account.Account{}).Return(nil)
+	repo := account.NewMockAccountRepository(ctrl)
+	uc := usecase.NewAccountUseCase(repo)
+	repo.EXPECT().Create(ctx, &account.Account{}).Return(nil)
+	uc.Create(ctx, &account.Account{})
 }
